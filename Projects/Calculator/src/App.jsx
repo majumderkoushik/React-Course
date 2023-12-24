88fdc5b63414054cb513dc6e7de4903d403aaac7
majumderkoushik <majumderkoushikme@gmail.com>
2023-12-25 02:56:25 +0530

    6th commit

--- Projects/Calculator/src/App.jsx
+++ Projects/Calculator/src/App.jsx
@@ -5,11 +5,22 @@ import ButtonsContainer from './components/ButtonsContainer'
 
 function App() {
   
-
+const[calVal, setCalVal] = useState("");
+const onButtonClick = (buttonText) => {
+  if( buttonText === 'c') {
+  setCalVal('');
+  }else if (buttonText === '='){
+   const result = eval(calVal);
+   setCalVal(result);
+  }else {
+    const newDisplayValue= calVal + buttonText;
+    setCalVal(newDisplayValue);
+  }
+};
   return (
     <div className={styles.calculator}>
-     <Display />
-     <ButtonsContainer></ButtonsContainer>
+     <Display displayVal={calVal} />
+     <ButtonsContainer onButtonClick = {onButtonClick}></ButtonsContainer>
     </div>
   )
 }
--- Projects/Calculator/src/components/ButtonsContainer.jsx
+++ Projects/Calculator/src/components/ButtonsContainer.jsx
@@ -1,16 +1,14 @@
 import styles from './ButtonsContainer.module.css'
 
 
-const ButtonsContainer = () => {
+const ButtonsContainer = ({onButtonClick}) => {
 
     const buttonNames = ['c','1','2','+','3','4','5','6','*','7','8','/','=','9','0','.'];
     
     return (
         <div className={styles.buttonsContainer}>
-        {buttonNames.map(buttonName => <button className={styles.button}>{buttonName}</button>)}
-        
-         
-       </div>
+        {buttonNames.map(buttonName => <button className={styles.button} onClick={() =>onButtonClick(buttonName)}>{buttonName}</button>)}
+        </div>
     )
 
 }
