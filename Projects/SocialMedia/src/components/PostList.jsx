43aa92d21944e3a02f90bc470fd872df8d4f8bef
majumderkoushik <majumderkoushikme@gmail.com>
2024-01-04 01:51:55 +0530

    commit

--- Projects/SocialMedia/src/components/PostList.jsx
+++ Projects/SocialMedia/src/components/PostList.jsx
@@ -1,11 +1,26 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import Post from './Post'
 import { PostList as PostListData } from '../store/post-list-store'
+import WelcomeMessage from './WelcomeMessage'
 
 function PostList() {
-    const {postList} = useContext(PostListData)
+    const {postList, addIntialPosts} = useContext(PostListData)
+    const [dataFetched, setDataFetched] = useState(false);
+
+    if(!dataFetched) {
+      fetch('https://dummyjson.com/posts')
+      .then(res => res.json())
+       .then(data => {
+         addIntialPosts(data.posts);
+       });
+       setDataFetched(true);
+    }
+    
+  
+    
   return (
     <>
+    {postList.length === 0 && <WelcomeMessage  />}
     {postList.map ((post)=> (
     <Post key={post.id} post ={post}/>
     ))}
--- Projects/SocialMedia/src/store/post-list-store.jsx
+++ Projects/SocialMedia/src/store/post-list-store.jsx
@@ -5,6 +5,7 @@ import { createContext, useReducer } from "react";
 export const PostList= createContext({
     postList: [],
     addPost: () => {},
+    addIntialPosts : () => {},
     deletePost: () => {},
 });
 
@@ -13,14 +14,17 @@ const postListReducer = (currPostList , action) => {
     let newPostList= currPostList;
     if(action.type=== 'DELETE_POST') {
    newPostList =currPostList.filter(post => post.id !== action.payload.postId);
-    } else if (action.type === 'ADD_POST') {
+    } else if (action.type ==='ADD_INITIAL_POSTS') {
+        newPostList = action.payload.posts;
+    } 
+    else if (action.type === 'ADD_POST') {
         newPostList = [action.payload, ...currPostList];
     }
     return newPostList;
 }
 
 const PostListProvider= ({children}) => {
-    const[postList, dispatchPostList] = useReducer(postListReducer, DEFAULT_POST_LIST);
+    const[postList, dispatchPostList] = useReducer(postListReducer,[]);
 
     const addPost = (userId, postTitle, postBody, reactions, tags) => {
 
@@ -36,6 +40,15 @@ const PostListProvider= ({children}) => {
         }
    })
     }
+    const addIntialPosts = (posts) => {
+
+        dispatchPostList({
+         type : 'ADD_INITIAL_POSTS',
+         payload : {
+                 posts
+             },
+        })
+         }
     const deletePost = (postId) => {
    dispatchPostList({
     type : "DELETE_POST",
@@ -45,28 +58,11 @@ const PostListProvider= ({children}) => {
    });
     }
     return (
-    <PostList.Provider value={{postList,addPost,deletePost}}>
+    <PostList.Provider value={{postList,addPost,addIntialPosts,deletePost}}>
     {children}
     </PostList.Provider>
     )
 }
 
-const DEFAULT_POST_LIST= [
-{
-id : '1',
-title: 'Going to Mumbai',
-body: 'Hi Friends, I am going to Mumbai for my vacations. Hope to enjoy a lot. Peace out.',
-reactions: 2,
-userId: 'user-9',
-tags: ["vacation", "Mumbai", "Enjoying"]
-},
-{
-id : '2',
-title: 'Pass ho gaya bhai',
-body: 'Hi Friends, I am going to Mumbai for my vacations. Hope to enjoy a lot. Peace out.',
-reactions: 15,
-userId: 'user-12',
-tags: ["graduating", "Unbelievable"]
-}
-]
+
 export default PostListProvider;
